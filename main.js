71a74324c41265c09be8a568a365ea8d75e82f4e
Liam Miller <LiamJmillerNZ@gmail.com>
2023-07-16 13:46:08 +1200

    Updated code

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-var weddingDate = new Date("2024-03-15T15:00:00"); // Date and time of the wedding
+document.addEventListener("DOMContentLoaded", function () {
+  const weddingDate = new Date("2024-03-15T15:00:00"); // Date and time of the wedding
 
-function updateCountdown() {
-  var now = new Date();
-  var difference = weddingDate - now;
+  function updateCountdown() {
+    let now = new Date();
+    let difference = weddingDate - now;
 
-  var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  var hours = Math.floor(
-    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    if (difference < 0) {
+      // Hide countdown and display a message
+      document.getElementById("countdown").style.display = "none";
+      document.getElementById("time-up").style.display = "block";
+      return;
+    }
 
-  document.getElementById("days").textContent = days;
-  document.getElementById("hours").textContent = hours;
-  document.getElementById("minutes").textContent = minutes;
-}
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-setInterval(updateCountdown, 1000); // Update countdown every second
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+  }
+
+  setInterval(updateCountdown, 1000); // Update countdown every second
+});
